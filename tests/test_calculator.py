fc03c3e4e690c955abdae9819f028fc3ad05275e
profrafaellamatos <78425843+profrafaellamatos@users.noreply.github.com>
2021-07-15 10:34:55 -0300

    Test divide and multiply

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,3 +7,11 @@ def test_add():
 
 def test_subtract():
     assert calculator.subtract(1, 2) == -1
+    
+
+def test_div():
+    assert calculator.divide(4, 2) == 2.0
+
+    
+def test_mult():
+    assert calculator.multiply(4, 2) == 8
